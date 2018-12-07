b08ee7b8040ab0917a87ace281e1b2fa8f96ef83
vic <viktoras.jefimovas@gmail.com>
2018-12-07 21:35:43 +0200

    added data providers

--- tests/NfqTeamsTest.php
+++ tests/NfqTeamsTest.php
@@ -7,78 +7,86 @@ use PHPUnit\Framework\TestCase;
 
 class NfqTeamsTest extends TestCase
 {
+    /** @var NfqTeams */
+    private $twoTeams = null;
+
+
+    //setting $twoTeams, function firstly called when test is called
+    public function setUp()
+    {
+        $this->twoTeams = new NfqTeams(
+            [
+                'academyui' => [
+                    'mentors' => ['Jonas Jonaitis'],
+                    'students' => ['Petras Petraitis', 'Gedas Gražauskas'],
+                ],
+                'supperreal' => [
+                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
+                    'students' => ['Vytautas Vėjūnas'],
+                ],
+            ]
+        );
+    }
+
+
     public function testEmpty()
     {
         $teams = new NfqTeams([]);
-        $this->assertNull($teams->getTeamByMember('not Existing'));
-        $this->assertNull($teams->getTeamByMentor('not Existing'));
+        $this->assertNull($teams->getTeamByMember('notExisting'));
+        $this->assertNull($teams->getTeamByMentor('notExisting'));
     }
 
-    public function testValidMentor()
+
+    public function providerValidMentors()
     {
-        $teams = new NfqTeams(
-            [
-                'academyui' => [
-                    'mentors' => ['Jonas Jonaitis'],
-                    'students' => ['Petras Petraitis', 'Gedas Gražauskas'],
-                ],
-                'supperreal' => [
-                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
-                    'students' => ['Vytautas Vėjūnas'],
-                ],
-            ]
-        );
-        $this->assertEquals('academyui', $teams->getTeamByMentor('Jonas Jonaitis'));
-        $this->assertEquals('supperreal', $teams->getTeamByMentor('Ada Kalbenė'));
+        return [
+            'one of one' => ['academyui', 'Jonas Jonaitis'],
+            'last one' => ['supperreal', 'Ada Kalbenė']
+        ];
     }
 
-    public function testValidMember(){
-        $teams = new NfqTeams(
-            [
-                'academyui' => [
-                    'mentors' => ['Jonas Jonaitis'],
-                    'students' => ['Petras Petraitis', 'Gedas Gražauskas'],
-                ],
-                'supperreal' => [
-                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
-                    'students' => ['Vytautas Vėjūnas'],
-                ],
-            ]
-        );
-        $this->assertEquals('academyui', $teams->getTeamByMember('Gedas Gražauskas'));
-        $this->assertEquals('supperreal', $teams->getTeamByMember('Vytautas Vėjūnas'));
+    public function providerValidMembers()
+    {
+        return [
+            'one of one' => ['supperreal', 'Vytautas Vėjūnas'],
+            'last one' => ['academyui', 'Gedas Gražauskas']
+        ];
     }
+
+
+    /**
+     * @dataProvider providerValidMentors
+     * @param string $expectedTeam
+     * @param string $actualName
+     */
+     public function testValidMentor($expectedTeam, $actualName)
+    {
+        $teams = $this->twoTeams;
+        $this->assertEquals($expectedTeam, $teams->getTeamByMentor($actualName));
+    }
+
+
+    /**
+     * @dataProvider providerValidMembers
+     * @param string $expectedTeam
+     * @param string $actualName
+     */
+    public function testValidMember($expectedTeam, $actualName)
+    {
+        $teams = $this->twoTeams;
+        $this->assertEquals($expectedTeam, $teams->getTeamByMember($actualName));
+    }
+
 
     public function testInvalidMember()
     {
-        $teams = new NfqTeams(
-            [
-                'academyui' => [
-                    'mentors' => ['Jonas Jonaitis'],
-                    'students' => ['Petras Petraitis', 'Gedas Gražauskas'],
-                ],
-                'supperreal' => [
-                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
-                    'students' => ['Vytautas Vėjūnas'],
-                ],
-            ]
-        );
+        $teams = $this->twoTeams;
         $this->assertNull($teams->getTeamByMember('Neegzistuojantis'));
     }
+
     public function testInvalidMentor()
     {
-        $teams = new NfqTeams(
-            [
-                'academyui' => [
-                    'mentors' => ['Jonas Jonaitis'],
-                    'students' => ['Petras Petraitis', 'Gedas Gražauskas'],
-                ],
-                'supperreal' => [
-                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
-                    'students' => ['Vytautas Vėjūnas'],
-                ],
-            ]
-        );
+        $teams = $this->twoTeams;
         $this->assertNull($teams->getTeamByMentor('Neegzistuojantis'));
     }
 }
