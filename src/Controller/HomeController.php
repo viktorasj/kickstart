30f68d66a42d4e2cfe132b29729f491d6911a23c
vic <viktoras.jefimovas@gmail.com>
2018-12-07 21:50:50 +0200

    added data providers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Services\TeamsInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/{member}", name="home")
      */
-    public function index()
+    public function index(string $member = "")
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/member/{member}", name="member")
+     */
+    public function member(TeamsInterface $teams, string $member = "")
+    {
+        return $this->render('home/member.html.twig', [
+            'member' => $member,
+            'team' => $teams->getTeamByMember($member)
         ]);
     }
 }
